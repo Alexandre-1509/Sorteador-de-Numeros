eabf75b0ef92c91a61a41adb87d45141338516a3
AlexandreSoares <soaresallexandre28@gmail.com>
2025-09-16 17:09:27 -0300

    Adicionado validação caso os dois inputs sejam preenchidos com o mesmo valor.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const buttonRandom = document.querySelector('.button-random')
-const mensage = document.querySelector('.mensage')
-const drawnNumber = document.querySelector('.drawn-number')
+const buttonRandom = document.querySelector(".button-random");
+const mensage = document.querySelector(".mensage");
+const drawnNumber = document.querySelector(".drawn-number");
 
 function clicou() {
-    const firstValue = document.querySelector('.first-value').value;
-    const secondValue = document.querySelector('.second-value').value;
+  const firstValue = document.querySelector(".first-value").value;
+  const secondValue = document.querySelector(".second-value").value;
 
-    const randomNumber = Math.random() * (firstValue, secondValue)
+  if (firstValue == secondValue) {
+    mensage.innerHTML = "Os números não poder ser iguais"
+
+  } else {
+    const randomNumber = Math.random() * (firstValue, secondValue);
     const result = randomNumber.toFixed(0);
-    mensage.innerHTML = 'O número sorteador foi: <br>'
-    drawnNumber.innerHTML = result
+    mensage.innerHTML = "O número sorteador foi: <br>";
+    drawnNumber.innerHTML = result;
+  }
 }
 
-buttonRandom.addEventListener('click', clicou)
+buttonRandom.addEventListener("click", clicou);
